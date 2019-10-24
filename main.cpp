cff2352a24eca75e58bf18e235b4180742191575
= <vsalexey@yandex.ru>
2019-10-24 15:29:35 +0300

    переделан файлик

--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 
+int return_two()
+{
+    return 2;
+}
+
 int main()
 {
     cout<<"TEST!"<<endl;
-    cout<<"----ПРОВЕРКА АВТОКОМПИЛЯЦИИ -----"<<endl;
+    cout<<return_two()<<endl;
     return 0;
 }
+
+
 
